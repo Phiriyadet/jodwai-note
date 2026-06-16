f585329752f27fd2be51cf2d9ed5dfd76607d8b1
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2026-06-16 14:39:50 +0700

    feat(frontend): add main layout and navbar components
    Added new files for the main layout and navbar components using React Router.

---
+++ frontend/JodWai-Web/src/components/layout/Navbar.tsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="border-b">
+      <div>
+        <div className="container mx-auto flex gap-4 p-4">
+          <NavLink to="/">Notes</NavLink>
+
+          <NavLink to="/about">About</NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+}
