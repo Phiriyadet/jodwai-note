76a67684b9cbdab6f7f790d6d62578101deb4792
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2026-06-17 11:31:48 +0700

    feat(frontend, nav): add graph page route and navigation item

--- frontend/JodWai-Web/src/app/router/index.tsx
+++ frontend/JodWai-Web/src/app/router/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../../components/layout/MainLayout";
 import NotesPage from "../../pages/NotePage";
 import AboutPage from "../../pages/AboutPage";
+import GraphPage from "../../pages/GraphPage";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <NotesPage />,
+      },
+      {
+        path: "/graph",
+        element: <GraphPage />,
       },
       {
         path: "/about",
--- frontend/JodWai-Web/src/components/layout/Navbar.tsx
+++ frontend/JodWai-Web/src/components/layout/Navbar.tsx
@@ -7,6 +7,8 @@ export default function Navbar() {
         <div className="container mx-auto flex gap-4 p-4">
           <NavLink to="/">Notes</NavLink>
 
+          <NavLink to="/graph">Graph</NavLink>
+
           <NavLink to="/about">About</NavLink>
         </div>
       </div>
