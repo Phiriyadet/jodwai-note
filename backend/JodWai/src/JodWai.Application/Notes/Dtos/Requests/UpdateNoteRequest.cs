1d5a348c4a596e78c39f24ca875c242bf0108e9d
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2026-07-06 11:13:13 +0700

    refactor(backend, requests): update request DTOs to use `JsonElement` for content
    - Replace `string` with `JsonElement` in `CreateNoteRequest` and `UpdateNoteRequest` for more flexible content storage.
    - Add necessary using directive for `System.Text.Json`.

--- backend/JodWai/src/JodWai.Application/Notes/Dtos/Requests/UpdateNoteRequest.cs
+++ backend/JodWai/src/JodWai.Application/Notes/Dtos/Requests/UpdateNoteRequest.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
+
 namespace JodWai.Application.Notes.Dtos.Requests;
 
 public record UpdateNoteRequest
-(Guid Id, string Title, string Content);
+(Guid Id, string Title, JsonElement Content);
